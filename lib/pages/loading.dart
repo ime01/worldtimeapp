e8530b569f7a55d9320d1a4e3311681b847ad781
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-07-05 00:24:22 -0700

    get request with http flutter package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Loading extends StatefulWidget {
@@ -13,11 +13,23 @@ class _LoadingState extends State<Loading> {
 
   void getData() async{
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
 
-    print(data);
-    print(data['title']);
+    // // Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    // Response response = await get(url) ;
+    // Map data = jsonDecode(response.body);
+    //
+    // print(data);
+    // print(data['title']);
+
+
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    // print(await http.get('https://jsonplaceholder.typicode.com/todos/1'));
+
+
 
     // //  simulate network request for a username
     // String username = await Future.delayed(Duration(seconds: 3), (){
